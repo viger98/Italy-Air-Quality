3ee00fc2960f14ccc9dd524b6546938138572e95
viger98 <44465510+viger98@users.noreply.github.com>
2019-01-29 16:48:32 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 $(function () {
-
+	
 	$.getJSON(
 		"https://api.myjson.com/bins/10lp8c",
 		
 		function(data){
 			console.log(data)
-    }
-  )
+			
+			let t = Handlebars.compile('<option value="{{ Territory }}">{{ Territory }}</option>');
+			data.forEach(function (el) {
+				$("#selezione").append(t(el));
+			})
+
+			$("#selezione").change(function () {
+				let paeseSel = document.getElementById('selezione').value;
+				let labels = []; 	
+				let test = [];		
+				console.log(paeseSel)
+				for (let i = 0; i < data.length; i++) {   	
+					if (paeseSel == data[i].Territory) {
+						labels.push(paeseSel);  	
+						test.push(data[i].Value); 	
+						console.log(data[i].Value)
+						
+					}
+				} 
+				
+			})		
+		})
 });
